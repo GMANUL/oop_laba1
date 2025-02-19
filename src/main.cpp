1521abe16a77bd57248bff75826c02e9ed428f37
GMANUL <142923862+GMANUL@users.noreply.github.com>
2025-02-20 00:45:42 +0300

    Initial commit: CMake project setup

---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "utils.cpp"
+
+int main() {
+    int a = sum(3, 5);
+    std::cout << a;
+}
---
+++ src/utils.cpp
@@ -0,0 +1,9 @@
+int sum(int first, int second){
+    return first + second;
+}
+double sum(double first, double second){
+    return first + second;
+}
+int cmp(int first, int second){
+    return first == second;
+}
